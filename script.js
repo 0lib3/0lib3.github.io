9aeab92a90a29a206ce4cdae78afd5e819be7b0a
Olive Belcher <130253308+0lib3@users.noreply.github.com>
2025-01-23 01:17:57 -0500

    Fixing contact

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ function sendMail(){
         message : document.getElementById("message").value
     }
 
-    emailjs.send("service_39xrlgo","template_sbawup5",parms).then(alert("Email Sent!"))
+    emailjs.send("service_39xrlgo","template_sbawup5",parms).then(alert("Email Sent! Please be sure you included your email/phone in the message field."))
 }
