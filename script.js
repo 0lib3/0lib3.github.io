c11fab5b79cf8de9157e003a94e9ea291af34ea0
Olive Belcher <130253308+0lib3@users.noreply.github.com>
2025-01-23 01:22:52 -0500

    Fix?

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function sendMail(){
     let parms = {
         name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
         subject : document.getElementById("subject").value,
         message : document.getElementById("message").value
     }
 
-    emailjs.send("service_39xrlgo","template_sbawup5",parms).then(alert("Email Sent! Please be sure you included your email/phone in the message field."))
+    emailjs.send("service_39xrlgo","template_sbawup5",parms).then(alert("Email Sent!"))
 }
